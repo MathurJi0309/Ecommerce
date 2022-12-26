7903c48bbda130503f0ba5d2ecc866cb0684e784
MathurJi <anileshjjssacademy84@gmail.com>
2022-12-27 00:01:01 +0530

    add the styling to the addproduct component and also work on the detail component

--- src/Components/AddProduct.js
+++ src/Components/AddProduct.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import {useNavigate} from "react-router-dom";
 import { toast } from "react-toastify";
 
 const AddProduct = () => {
@@ -7,25 +8,39 @@ const [title,setTitle]=useState('');
 const [src,setSrc]=useState('');
 const [price,setPrice]=useState('');
 const [discription,setDiscription]=useState('');
+const [add,setAdd] =useState(false);
+const navigate=useNavigate()
 
-const handleadd=(e)=>{
-    e.preventdefault()
+const handleadd=()=>{
+    // e.preventdefault()
+    
 
     toast.info(`Product is deleted`,{
         position:"top-right",
     });
+    setAdd(true)
+    navigate("/")
 
 }
 
-    return (  
-        <>
-        <form className="">
+    return (
+        
+        
+        <div>
+        {
+            add?(
+                <h1>hi i am here</h1>
+            ):(
+                <div className="input">
+            <h1 >Add New Product here</h1>
+        <form className="addproduct">
             <input
             type="text"
             value={title}
             name="title"
             placeholder="type here the title of the product"
             onChange={(e)=>{setTitle(e.target.value)}}
+            className="titleinput"
             />
             <input
             type="text"
@@ -33,13 +48,15 @@ const handleadd=(e)=>{
             name="src"
             placeholder="enter here the url of the product"
             onChange={(e)=>{setSrc(e.target.value)}}
-            />
+            className="imginput"/>
+
             <input
             type="text"
             value={price}
             name="price"
             placeholder="enter the price of the product"
             onChange={(e)=>{setPrice(e.target.value)}}
+            className="titleinput"
             />
             <input
             type="text"
@@ -47,12 +64,16 @@ const handleadd=(e)=>{
             name="discription"
             placeholder="enter discription of product"
             onChange={(e)=>{setDiscription(e.target.value)}}
+            className="titleinput"
             />
-            <button onClick={(e)=>{handleadd()}}>
+            <button onClick={()=>handleadd}>
                 Add Product
             </button>
         </form>
-        </>
+        </div>
+            )
+        }
+        </div>
     );
 }
  
--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -6,10 +6,17 @@ const Detail = () => {
     return (  
         <>
         <div>
-            <h1>
-                <h1>{details.detailpro[0].title}</h1>
-                <img src={details.detailpro[0].thumbnail}/>
-            </h1>
+            <div className="productdetail">
+                <h1 className="producttitle">{details.detailpro[0].title}</h1>
+                <img className="productimg" src={details.detailpro[0].thumbnail} alt={details.detailpro[0].title}/>
+                <h1>{details.detailpro[0].description}</h1>
+                <h1>{details.detailpro[0].stock}</h1>
+                <h1>{details.detailpro[0].price}</h1>
+                <h1>{details.detailpro[0].discountPercentage}</h1>
+                <h1>{details.detailpro[0].rating}</h1>
+                <h1>{details.detailpro[0].category}</h1>
+                <h1>{details.detailpro[0].brand}</h1>
+            </div>
         </div>
         </>
     );
